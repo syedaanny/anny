5aa3c036c20c8ad637db9baa50b0181b58d6878c
Syeda Anny Akther <syedaanny02@gmail.com>
2023-08-20 18:01:20 +0600

    anny

---
+++ src/Components/HomeSection.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import styles from '@/styles/Navbar.css';
+
+const HomeSection = () => {
+  return (
+    <section className={styles.homeSection}>
+      <div className={styles.flexContainer}>
+        {/* Your content with flex display */}
+      </div>
+    </section>
+  );
+};
+
+export default HomeSection;
---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Navbar from '@/Components/Navbar';
+import HomeSection from '@/Components/HomeSection';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    
+      <HomeSection />
+    </div>
+  );
+};
+
+export default Home;
